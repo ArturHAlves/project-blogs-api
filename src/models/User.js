b4e08a4254746749f541c244555bb783f7a82142
Artur Henrique <arthenrdev@gmail.com>
2023-06-12 16:21:55 -0300

    feat: Req02 - Criação do Model User

---
+++ src/models/User.js
@@ -0,0 +1,41 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define(
+    'User',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      displayName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      image: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      tableName: 'users',
+      timestamps: false,
+      underscored: true,
+    }
+  );
+
+  return User;
+};
