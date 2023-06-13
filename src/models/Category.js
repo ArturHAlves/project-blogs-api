efb87b1e4af181988316f7a60f8a2e035d51c864
Artur Henrique <arthenrdev@gmail.com>
2023-06-13 10:37:17 -0300

    feat: Req07 - Criação Model Category

---
+++ src/models/Category.js
@@ -0,0 +1,29 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
+
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define(
+    'Category',
+    {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+    },
+    {
+      tableName: 'categories',
+      timestamps: false,
+      underscored: true,
+    }
+  );
+
+  return Category;
+};
